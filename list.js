93d419bf8da69e98071ca78b420e67a051ec9f81
Joshua Balao <joshuacbalao@gmail.com>
2020-06-17 22:38:20 -0400

    add new files to git hub

---
+++ list.js
@@ -0,0 +1,8 @@
+//add randomword npm to create list of words//
+
+var listOfWords = [
+    'lightning mcqueen', 'dash', 'mr incredible', 'flick', 'mickey mouse', 'minnie mouse',
+    'buzz lightyear', 'woody', 'snow white', 'cinderella', 'stitch', 'jessie', 
+    'donald duck', 'goofy', 'daisy', 'pete', 'pluto', 'goliath', 'hudson', 
+
+]
